e9e9a2bfff4332f0929ce6eb762a7f5ff01089e6
Christian Camilo Herrera Pineda <cristianc.herrerap@gmail.com>
2024-08-30 01:13:10 -0500

    adicion campos especificos getOne Aristoteles m5 v1

--- src/infraestructure/datasource/curso.datasource.ts
+++ src/infraestructure/datasource/curso.datasource.ts
@@ -2,6 +2,13 @@ import { CursosEntityApplication } from "../../domain/entities";
 import { CursoRepository } from "../../domain/repositories";
 import { CustomError } from "../../domain/errors/custom.error";
 import Curso from "../../data/sequelize/models/cursos.model";
+import Nivel from "../../data/sequelize/models/niveles.model";
+import Instructor from "../../data/sequelize/models/instructores.model";
+import Resena from "../../data/sequelize/models/resena.model";
+import Modulo from "../../data/sequelize/models/modulos.model";
+import User from "../../data/sequelize/models/user.model";
+import Role from "../../data/sequelize/models/rol.model";
+import Calificacion from "../../data/sequelize/models/calificaciones.model";
 
 
 
@@ -9,7 +16,67 @@ import Curso from "../../data/sequelize/models/cursos.model";
 
 export class CursoDataSource implements CursoRepository {
     async getOne(id: number): Promise<CursosEntityApplication> {
-        const curso = await Curso.findOne({where: { id }});
+        const curso = await Curso.findOne({      
+            attributes: [
+            "id",
+            "titulo",
+            "descripcion",
+            "objetivos",
+            "duracion",
+            "nivel_id",
+            "instructor_id",
+            "fecha_inicio",
+            "resena_id",
+            "foto"
+          ],
+          include: [
+            {
+                as: "modulo",
+                attributes: ["id", "modulo"],
+                model: Modulo
+            },
+            {
+                as: "user",
+                attributes: ["id", "names", "surnames", "email", "date_birth", "role_id", "password"],
+                model: User,
+                include: [{
+                    as: "role",
+                    attributes: ["id", "name", "description"],
+                    model: Role
+                }]
+            },
+            {
+                as: "nivel",
+                attributes: ["id", "nivel"],
+                model: Nivel
+            },
+            {
+                as: "instructor",
+                attributes: ["id", "nombre", "apellido"],
+                model: Instructor
+            },
+            {
+                as: "resena",
+                attributes: ["id", "calificacion_id", "resena", "user_id"],
+                model: Resena,
+                include: [{
+                    as: "calificacion",
+                    attributes: ["id", "calificacion"],
+                    model: Calificacion
+                },
+                {
+                    as: "user",
+                    attributes: ["id", "names", "surnames", "email", "date_birth", "role_id", "password"],
+                    model: User,
+                    include: [{
+                        as: "role",
+                        attributes: ["id", "name", "description"],
+                        model: Role
+                    }]
+                }
+            ]
+            }
+        ], where: { id }});
       
         if (!curso) throw CustomError.badRequest("El Curso no Existe");
       
